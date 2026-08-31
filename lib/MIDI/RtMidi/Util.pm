adc38d7f41546c2b3e85ab362a612ca5e31e8612
Gene Boggs <gene.boggs@gmail.com>
2026-08-31 16:49:56 -0700

    Halt

--- lib/MIDI/RtMidi/Util.pm
+++ lib/MIDI/RtMidi/Util.pm
@@ -5,8 +5,17 @@ package MIDI::RtMidi::Util;
 our $VERSION = '0.0100';
 
 use v5.36;
+use feature 'try';
 
+use Data::Dumper::Compact qw(ddc);
 use MIDI::RtMidi::FFI::Device ();
+use Exporter 'import';
+our @EXPORT = qw(
+    out_port
+    halt
+);
+
+no warnings 'experimental::try';
 
 =head1 SYNOPSIS
 
@@ -32,6 +41,7 @@ Open and return a L<MIDI::RtMidi::FFI::Device> C<RtMidiOut> device.
 
 sub out_port ($name) {
     my $midi_out = RtMidiOut->new;
+    say ddc $midi_out;
     try { $midi_out->open_virtual_port('RtMidiOut') } # needed for mac
     catch ($e) {
         # warn 'Not a Mac';
@@ -39,6 +49,25 @@ sub out_port ($name) {
     try { $midi_out->open_port_by_name(qr/\Q$name/i) }
     catch ($e) { die "Can't open MIDI port: $name\n" }
     return $midi_out;
+}
+
+=head2 halt
+
+  halt();
+
+Stop and close an open C<RtMidiOut> device.
+
+=cut
+
+sub halt ($midi_out) {
+    try {
+        $midi_out->stop;
+        $midi_out->panic;
+    }
+    catch ($e) {
+        warn "Can't halt the MIDI out devices: $e\n";
+    }
+    exit;
 }
 
 1;
