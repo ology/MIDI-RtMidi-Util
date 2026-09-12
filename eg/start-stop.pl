972b3af38fb2102a7b16807385d56aa8add91ce8
Gene Boggs <gene.boggs@gmail.com>
2026-09-11 19:19:33 -0700

    Fresh

--- eg/start-stop.pl
+++ eg/start-stop.pl
@@ -7,7 +7,6 @@ use v5.36;
 use Data::Dumper::Compact qw(ddc);
 use IO::Async::Loop ();
 use IO::Async::Timer::Periodic ();
-use MIDI::RtMidi::FFI::Device ();
 use MIDI::RtMidi::Util qw(out_port stop_device stop_all_notes);
 
 my $port = shift || 'usb';
